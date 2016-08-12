0ad55c21d67cced6915203530ce526166ea58baf
Timothy Herold <heroldt@gmail.com>
2016-08-12 11:14:32 -0700

    Add javascript to weigh the user's answers, determine which track is best, and display that result.

---
+++ js/scripts.js
@@ -0,0 +1,125 @@
+var first_name;
+var last_name;
+$(document).ready(function() {
+  $("form#user-input").submit(function(event){
+    firstName = $("#first_name").val();
+    lastName = $("#last_name").val();
+    /* Get values from user responses. */
+    var firstAnswer = $("input:radio[name=question1]:checked").val();
+    var secondAnswer = $("input:radio[name=question2]:checked").val();
+    var thirdAnswer = $("input:radio[name=question3]:checked").val();
+    var fourthAnswer = $("input:radio[name=question4]:checked").val();
+    var fifthAnswer = $("input:radio[name=question5]:checked").val();
+
+    /* We're going to assign an int variable to each epicodus track. Then we'll weigh the answers from the user. Some responses are worth more than others.
+    At the end we'll determine which track received the highest score, and display that result */
+    var rubyRails = 0;
+    var phpDrupal = 0;
+    var javaAndroid = 0;
+    var cssDesign = 0;
+    var csharpDotnet = 0;
+
+    /* Weighing responses from first question */
+    if (firstAnswer === 1) {
+
+    } else if (firstAnswer === 2) {
+
+    } else if (firstAnswer === 3) {
+
+    } else if (firstAnswer === 4) {
+
+    }
+
+    /* Weighing responses from second question */
+    if (secondAnswer === 1) {
+
+    } else if (secondAnswer === 2) {
+
+    } else if (secondAnswer === 3) {
+
+    } else if (secondAnswer === 4) {
+
+    }
+
+    /* Weighing responses from third question */
+    if (thirdAnswer === 1) {
+
+    } else if (thirdAnswer === 2) {
+
+    } else if (thirdAnswer === 3) {
+
+    } else if (thirdAnswer === 4) {
+
+    }
+
+    /* Weighing responses from fourth question */
+    if (fourthAnswer === 1) {
+
+    } else if (fourthAnswer === 2) {
+
+    } else if (fourthAnswer === 3) {
+
+    } else if (fourthAnswer === 4) {
+
+    }
+
+    /* Weighing responses from fifth question */
+    if (fifthAnswer === 1) {
+
+    } else if (fifthAnswer === 2) {
+
+    } else if (fifthAnswer === 3) {
+
+    } else if (fifthAnswer === 4) {
+
+    }
+    
+    /* Now we determine which epicodus path has the greatest score and display that result. */
+    var max = Math.max(rubyRails, phpDrupal, javaAndroid, cssDesign, csharpDotnet);
+    /* Display the highest result. In the case of a tie, display both and change the verbage a bit. */
+    var resultNumber = 0;
+    if (rubyRails === max) {
+      displayResult(".ruby-rails", resultNumber);
+      resultNumber += 1;
+    }
+    if (phpDrupal === max) {
+      displayResult(".php-drupal", resultNumber);
+      resultNumber += 1;
+    }
+    if (javaAndroid === max) {
+      displayResult(".java-android", resultNumber);
+      resultNumber += 1;
+    }
+    if (cssDesign === max) {
+      displayResult(".css-design", resultNumber);
+      resultNumber += 1;
+    }
+    if (csharpDotnet === max) {
+      displayResult(".csharp-dotnet", resultNumber);
+      resultNumber += 1;
+    }
+    event.preventDefault();
+  });
+
+  $("#btn-retake").on("click", function(event) {
+    $(".result").hide();
+    $(".results").hide();
+    $("form#user-input")[0].reset();
+    $(".form").show();
+    event.preventDefault();
+  });
+});
+
+displayResult = function(result, resultNumber) {
+  $(".form").hide();
+  $(".first_name").text(firstName);
+  $(".last_name").text(lastName);
+  /* Change the verbage used based off whether this is the first result displayed, or any other result after the first one */
+  if (resultNumber === 0) {
+    $(result + " .result_number").text("ought to");
+  } else {
+    $(result + " .result_number").text("could also")
+  }
+  $(result).show();
+  $(".results").show()
+}
